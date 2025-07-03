b6b9fd2671554caabffe3861825cdebf35a4840d
emmamelkumian <147114444+emmamelkumian@users.noreply.github.com>
2025-07-02 17:54:52 -0700

    Delete software-engineering-project-team-simm-main directory

--- software-engineering-project-team-simm-main/test/mock/MockFile.js
+++
@@ -1,7 +0,0 @@
-export class MockFile extends Blob {
-	constructor(data, filename, options) {
-		super(data, options);
-		this.name = filename;
-		this.lastModified = options.lastModified || Date.now();
-	}
-}
